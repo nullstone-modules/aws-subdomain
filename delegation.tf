e51fbb3cfc53ce1b22c4a5565c1e00db4c71cb67
Brad Sickles <brad.sickles@gmail.com>
2020-09-08 14:18:44 -0400

    Setting up delegated zone records through delegated user

--- delegation.tf
+++ delegation.tf
@@ -1,6 +1,9 @@
 // We will need to be able to support secondary providers since the root domain
 //   is typically managed in a separate account from non-production environments
 provider "aws" {
+  access_key = data.terraform_remote_state.domain.outputs.delegator["access_key"]
+  secret_key = data.terraform_remote_state.domain.outputs.delegator["secret_key"]
+
   alias = "domain"
 }
 
