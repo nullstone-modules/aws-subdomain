0ad551fc42b56d62d4ee81182815d88fa023b8fa
Brad Sickles <brad.sickles@gmail.com>
2020-10-16 15:34:09 -0400

    Changing cert validation to timeout quicker

--- cert/main.tf
+++ cert/main.tf
@@ -40,6 +40,10 @@ resource "aws_route53_record" "cert_validation" {
 resource "aws_acm_certificate_validation" "this" {
   certificate_arn         = aws_acm_certificate.this.arn
   validation_record_fqdns = [for record in aws_route53_record.cert_validation : record.fqdn]
+
+  timeouts {
+    create = "5m"
+  }
 }
 
 output "certificate_arn" {
