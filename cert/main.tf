dc7ea55de81b39b98ff1ca802e4ea97a77c21d55
Brad Sickles <brad.sickles@gmail.com>
2021-01-05 12:16:17 -0500

    try again

--- cert/main.tf
+++ cert/main.tf
@@ -28,7 +28,7 @@ resource "aws_acm_certificate" "this" {
 
 resource "aws_route53_record" "cert_validation" {
   for_each = {
-    for dvo in aws_acm_certificate.this.domain_validation_options : dvo.domain_name => {
+    for dvo in (var.verify_by_email ? [] : aws_acm_certificate.this.domain_validation_options) : dvo.domain_name => {
       name   = dvo.resource_record_name
       record = dvo.resource_record_value
       type   = dvo.resource_record_type
